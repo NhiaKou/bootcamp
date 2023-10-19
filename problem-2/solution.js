4735e57ba2da265192577d2f8932dfde06d4baa6
panhiathao <panhiathao@siliconwat.com>
2023-10-19 15:08:41 -0700

    firstname done

--- problem-2/solution.js
+++ problem-2/solution.js
@@ -3,3 +3,15 @@
 Create a function that returns the name "Luna"
 
 */
+
+function firstName() {
+    return "Luna";
+}
+
+console.log(firstName())
+
+//this is chat code
+
+function getName() {
+    return "Luna";
+  }

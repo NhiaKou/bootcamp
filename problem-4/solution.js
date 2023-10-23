d74c8e1c8445bea8e3dbcfb46fc2822fbcd5a8d6
panhiathao <panhiathao@siliconwat.com>
2023-10-23 07:31:41 -0700

    problem 4 done

--- problem-4/solution.js
+++ problem-4/solution.js
@@ -1,3 +1,13 @@
 /*
-Create a function that takes in an input and returns that input.
+Create a function that takes in an input and returns that input.n  C
 */
+
+function takeIn(x) {
+    return x;
+}
+
+console.log(takeIn(1))
+
+function returnInput(input) {
+    return input;
+  }
